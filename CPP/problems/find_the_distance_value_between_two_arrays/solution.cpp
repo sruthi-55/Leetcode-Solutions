0c35ebc3a166981fbed3af1b926f1ebe35251189
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-02-10 13:24:50 +0000

    Sync LeetCode submission - Find the Distance Value Between Two Arrays (cpp)

---
+++ CPP/problems/find_the_distance_value_between_two_arrays/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        int c=0;
+        for(int i=0;i<arr1.size();i++){
+            bool found = false;
+            for(int j=0;j<arr2.size();j++){
+                if(abs(arr1[i]-arr2[j]) <=d){
+                    found=true;
+                    break;
+                } 
+            }
+            if(!found)  c++;
+        }
+        return c;
+    }
+};
