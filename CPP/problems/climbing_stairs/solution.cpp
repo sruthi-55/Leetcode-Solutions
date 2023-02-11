6ab1cc6c459df48efad96bcac549103ab676d866
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-02-11 16:10:14 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

---
+++ CPP/problems/climbing_stairs/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev2=1,prev=1;
+        for(int i=2;i<=n;i++){
+            int cur=prev+prev2;
+            prev2=prev;
+            prev=cur;
+        }   
+       
+       return prev;
+    }
+};
+
+
+
+     
