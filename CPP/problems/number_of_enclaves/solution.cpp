a999591a621762730171f3343e17666a5be6acbd
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-03-19 17:25:39 +0000

    Sync LeetCode submission - Number of Enclaves (cpp)

--- CPP/problems/number_of_enclaves/solution.cpp
+++ CPP/problems/number_of_enclaves/solution.cpp
@@ -13,6 +13,28 @@ public:
             }
         }
     }
+    void bfs(int sr,int sc,vector<vector<int>>& mat,vector<vector<int>>& vis){
+        int m=mat.size(),n=mat[0].size();
+        vis[sr][sc]=1;
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            int dR[]={-1,0,1,0};
+            int dC[]={0,1,0,-1};
+            for(int i=0;i<4;i++){
+                int nrow=r+dR[i];
+                int ncol=c+dC[i];
+                if(nrow>=0 && ncol>=0 && nrow<m && ncol<n &&
+                !vis[nrow][ncol] && mat[nrow][ncol]==1){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+    }
     int numEnclaves(vector<vector<int>>& grid) {
         int count=0;
         int m=grid.size(),n=grid[0].size();
@@ -21,7 +43,7 @@ public:
             for(int j=0;j<n;j++){
                 if(i==0 || i==m-1 || j==0 || j==n-1){
                     if(!vis[i][j] && grid[i][j]==1){
-                        dfs(i,j,grid,vis);
+                        bfs(i,j,grid,vis);
                     }
                 }
             }
