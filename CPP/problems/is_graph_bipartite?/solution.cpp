6b509ca833f558ead5728fc192eb8aa005a4b534
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-03-20 11:31:27 +0000

    Sync LeetCode submission - Is Graph Bipartite? (cpp)

--- CPP/problems/is_graph_bipartite?/solution.cpp
+++ CPP/problems/is_graph_bipartite?/solution.cpp
@@ -14,13 +14,49 @@ public:
         }
         return true;
     }
+    bool bfs1(int i,int col,vector<vector<int>> &graph,int color[]){
+        queue<int> q;
+        q.push(i);
+        color[i]=col;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it:graph[node]){
+                if(color[it]==-1){
+                    q.push(it);
+                    color[it]=!color[node];
+                }
+                else if(color[it]==color[node])   return false;
+            }
+        }
+        return true;
+    }
+
+    bool bfs(int i,int col,vector<vector<int>> &graph,int color[]){
+        queue<pair<int,int>> q;
+        q.push({i,col});
+        color[i]=col;
+        while(!q.empty()){
+            int node=q.front().first;
+            int nCol=q.front().second;
+            q.pop();
+            for(auto it:graph[node]){
+                if(color[it]==-1){
+                    q.push({it,!nCol});
+                    color[it]=!nCol;
+                }
+                else if(color[it]==nCol)   return false;
+            }
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph){
         int m=graph.size();
         int color[m];
         for(int i=0;i<m;i++)    color[i]=-1;
         for(int i=0;i<m;i++){
             if(color[i]==-1){
-                if(dfs(i,0,graph,color)==false){
+                if(bfs(i,0,graph,color)==false){
                     return false;
                 }    
             }
