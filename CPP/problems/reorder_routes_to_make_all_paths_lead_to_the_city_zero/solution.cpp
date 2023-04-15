47fbc94a9ab51e65fbfb885b9fedde43ab654408
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-04-15 06:14:17 +0000

    Sync LeetCode submission - Reorder Routes to Make All Paths Lead to the City Zero (cpp)

---
+++ CPP/problems/reorder_routes_to_make_all_paths_lead_to_the_city_zero/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dfs(int i,vector<vector<int>> &adj,vector<int> &vis){
+        int count=0;
+        vis[i]=1;
+        for(auto it:adj[i]){
+            if(!vis[abs(it)]){
+                count+=dfs(abs(it),adj,vis)+(it>0);
+            }
+        }
+        return count;
+    }
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> adj(n);
+        for (auto &c : connections) {
+            adj[c[0]].push_back(c[1]);
+            adj[c[1]].push_back(-c[0]);
+        }
+        vector<int> vis(n,0);
+        return dfs(0,adj,vis);
+    }
+};
