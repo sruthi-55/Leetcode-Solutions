f0236c1ead4d79417115e78aa6ed8b1a5a9d8226
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-06-01 04:39:36 +0000

    Sync LeetCode submission - Split a String in Balanced Strings (python)

---
+++ CPP/problems/split_a_string_in_balanced_strings/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def balancedStringSplit(self, s):
+        res = cnt = 0         
+        for c in s:
+            cnt += 1 if c == 'L' else -1            
+            if cnt == 0:
+                res += 1
+        return res  
