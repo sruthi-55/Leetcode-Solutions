f54541efd69d1122aae4a1b29059f1832805edff
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-06-21 15:09:15 +0000

    Sync LeetCode submission - Path In Zigzag Labelled Binary Tree (cpp)

--- CPP/problems/path_in_zigzag_labelled_binary_tree/solution.cpp
+++ CPP/problems/path_in_zigzag_labelled_binary_tree/solution.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    bool isPowerTwo(long long n){
-        return ((n - 1)&n) == 0;
-    }
-    int getLevelStart(long long n){
-        while(!isPowerTwo(n)){
-            n++;
-        }
-        return n/2;
-    }
-    int getParent(int n){
-        if(isPowerTwo(n)){
-            return n - 1;
-        } else {
-            int level_start = getLevelStart(n);
-            int difference = (n - level_start)/2;
-            return level_start - 1 - difference;
-        }
-    }
     vector<int> pathInZigZagTree(int label) {
         vector<int> result;
-        result.push_back(label);
-        while(label != 1){
-            int parent = getParent(label);
-            result.push_back(parent);
-            label = parent;
+        result.push_back(label); 
+        int height=log2(label); 
+
+        while(height!=0){
+            int left=pow(2,height-1); 
+            int right=pow(2,height)-1;
+            label=left+(right-label/2);
+            result.push_back(label); 
+            height--;
         }
-        reverse(result.begin() , result.end());
+        
+        reverse(result.begin(),result.end()); 
         return result;
     }
 };
+
