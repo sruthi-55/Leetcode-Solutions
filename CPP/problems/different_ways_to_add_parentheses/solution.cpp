9402029b4610f7466fdbfb0531f4253b6ede974e
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-07-04 16:21:54 +0000

    Sync LeetCode submission - Different Ways to Add Parentheses (cpp)

--- CPP/problems/different_ways_to_add_parentheses/solution.cpp
+++ CPP/problems/different_ways_to_add_parentheses/solution.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    int perform(int a,int b,char op){
-        if(op=='+') return a+b;
-        if(op=='-') return a-b;
-        if(op=='*') return a*b;
-        return 0;
-    }
-
-
     vector<int> diffWaysToCompute(string exp) {
-        vector<int> results;
-        bool isNumber=1;
-
-        
+        vector<int> res;
         for(int i=0;i<exp.length();i++){
-            if(!isdigit(exp[i])){
-                isNumber=0;
+            if(exp[i]=='*' || exp[i]=='-' || exp[i]=='+'){
                 vector<int> left= diffWaysToCompute(exp.substr(0,i));
                 vector<int> right= diffWaysToCompute(exp.substr(i+1));
                 for(int a:left){
                      for(int b:right){
-                        int val = perform(a,b,exp[i]);
-                        results.push_back(val);
+                        if(exp[i]=='+') res.push_back(a+b);
+                        else if(exp[i]=='-') res.push_back(a-b);
+                        else res.push_back(a*b);
                     }
                 }
-                   
             }
         }
-        if(isNumber==1) results.push_back(stoi(exp));
-        return results;
+        //if we find no operator
+        //ca't divide further
+        if(res.size()==0) res.push_back(stoi(exp));
+        return res;
         
     }
 };
