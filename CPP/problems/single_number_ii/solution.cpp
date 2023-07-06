2cb986c4e31f12799df7cc07a564d2849e1f1f23
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-07-06 08:25:16 +0000

    Sync LeetCode submission - Single Number II (cpp)

---
+++ CPP/problems/single_number_ii/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(int i=0;i<32;i++){
+            int cSet=0,cUnset=0;
+            for(int j=0;j<nums.size();j++){
+               if((1<<i)&nums[j])    cSet++;
+               else cUnset++;
+            }
+            if(cSet%3)   
+                ans=ans|(1<<i);
+        }
+        return ans;
+    }
+};
