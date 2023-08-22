7417042979a4299a59d2013e71fd1561573a0b7e
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-08-22 13:21:37 +0000

    Sync LeetCode submission - Reverse Only Letters (cpp)

--- CPP/problems/reverse_only_letters/solution.cpp
+++ CPP/problems/reverse_only_letters/solution.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
+    bool isLetter(char c){
+        c=tolower(c);
+        if(c>=97 && c<=122) return true;
+        return false;
+    }
     string reverseOnlyLetters(string s) {
-        int i=0,j=s.size()-1;
-        while(i<j){
-            if(!isalpha(s[i]))  i++;
-            else if(!isalpha(s[j])) j--;
-            else{
-                swap(s[i],s[j]);
-                i++;j--;
-            }
+        int l=0,h=s.size()-1;
+        while(l<h){
+            while(l<h && !isLetter(s[l]))  l++;
+            while(l<h && !isLetter(s[h]))  h--;
+            swap(s[l],s[h]);
+            l++;h--;
         }
         return s;
     }
