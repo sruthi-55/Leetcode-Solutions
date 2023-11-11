eaeec7dbec91fc783bab9a788cf19423724d0b2f
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-11-11 06:01:41 +0000

    Sync LeetCode submission - Maximum Binary Tree (cpp)

--- CPP/problems/maximum_binary_tree/solution.cpp
+++ CPP/problems/maximum_binary_tree/solution.cpp
@@ -11,19 +11,24 @@
  */
 class Solution {
 public:
-       TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        return helper(nums, 0, nums.size() - 1);
-    }
-    TreeNode* helper(vector<int>& nums, int left, int right){
-        if(left>right)return NULL;
-        int max_index = left;
-        for(int i = left; i<=right; i++){
-            if(nums[i] > nums[max_index])max_index = i; 
+    int getMaxIndex(vector<int>& nums,int left,int right){
+        int maxI = left;
+        for(int i=left;i<=right;i++){
+            if(nums[maxI]<nums[i])  maxI=i;
         }
+        return maxI;
+    }
+    TreeNode* solve(vector<int> &nums,int left,int right){
+        if(left>right)  return NULL;
         
-        TreeNode* root = new TreeNode(nums[max_index]);
-        root->left = helper(nums, left, max_index - 1);
-        root->right = helper(nums, max_index + 1, right);
+        int maxI = getMaxIndex(nums,left,right);
+        TreeNode *root = new TreeNode(nums[maxI]);
+
+        root->left = solve(nums,left,maxI-1);
+        root->right = solve(nums,maxI+1,right);
         return root;
+    }
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return solve(nums,0,nums.size()-1);
     }
 };
